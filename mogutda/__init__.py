e5ff1060eb45052d34f2ef7398536d88439948ce
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-04-10 17:43:58 -0400

    code cleanup

--- mogutda/__init__.py
+++ mogutda/__init__.py
@@ -1,31 +1,5 @@
-from itertools import combinations
 
-
-def facesiter(simplex):
-    for i in range(len(simplex)):
-        yield simplex[:i] + simplex[(i + 1) :]
-
-
-def flattening_simplex(simplices):
-    for simplex in simplices:
-        for point in simplex:
-            yield point
-
-
-def get_allpoints(simplices):
-    return set(flattening_simplex(simplices))
-
-
-def faces(simplices):
-    faceset = set()
-    for simplex in simplices:
-        numnodes = len(simplex)
-        for r in range(numnodes, 0, -1):
-            for face in combinations(simplex, r):
-                faceset.add(tuple(sorted(face)))
-    return faceset
-
-
+from .utils import *
 from .abssimcomplex import SimplicialComplex
 from .alphacomplex import AlphaComplex
 from .vrcomplex import VietorisRipsComplex
--- mogutda/alphacomplex.py
+++ mogutda/alphacomplex.py
@@ -2,17 +2,19 @@
 from scipy.spatial import Delaunay, distance
 
 from .abssimcomplex import SimplicialComplex
-from . import facesiter, get_allpoints
+from .utils import facesiter, get_allpoints
 
 
 def calculate_distmatrix(points, labels, distfcn):
-    return {(labels[i], labels[j]): distfcn(points[i], points[j])
-            for i in range(len(labels)) for j in range(len(labels))}
+    return {
+        (labels[i], labels[j]): distfcn(points[i], points[j])
+        for i in range(len(labels)) for j in range(len(labels))
+    }
 
 
 def contain_detachededges(simplex, distdict, epsilon):
     if len(simplex) == 2:
-        return (distdict[simplex[0], simplex[1]] > 2*epsilon)
+        return distdict[simplex[0], simplex[1]] > 2*epsilon
     else:
         for face in facesiter(simplex):
             contained = contain_detachededges(face, distdict, epsilon)
@@ -22,21 +24,23 @@ def contain_detachededges(simplex, distdict, epsilon):
 
 
 class AlphaComplex(SimplicialComplex):
-    def __init__(self,
-                 points,
-                 epsilon,
-                 labels=None,
-                 distfcn=distance.euclidean):
+    def __init__(
+            self,
+            points,
+            epsilon,
+            labels=None,
+            distfcn=distance.euclidean
+    ):
+        super(AlphaComplex, self).__init__()
         self.pts = points
         self.labels = (range(len(self.pts))
                        if (labels is None or len(labels) != len(self.pts))
                        else labels)
         self.epsilon = epsilon
         self.distfcn = distfcn
-        self.import_simplices(self.construct_simplices(self.pts,
-                                                       self.labels,
-                                                       self.epsilon,
-                                                       self.distfcn))
+        self.import_simplices(
+            self.construct_simplices(self.pts, self.labels, self.epsilon, self.distfcn)
+        )
 
     def construct_simplices(self, points, labels, epsilon, distfcn):
         distdict = calculate_distmatrix(points, labels, distfcn)
@@ -50,11 +54,15 @@ class AlphaComplex(SimplicialComplex):
             detached = [contain_detachededges(face, distdict, epsilon) for face in facesiter(simplex)]
 
             if True in detached and len(simplex) > 2:
-                simplices += [face for face, notkeep in zip(facesiter(simplex), detached)
-                              if not notkeep]
+                simplices += [
+                    face
+                    for face, notkeep in zip(facesiter(simplex), detached)
+                    if not notkeep
+                ]
             else:
                 simplices.append(simplex)
-        simplices = map(lambda simplex: tuple(sorted(simplex)), simplices)
+        # simplices = map(lambda simplex: tuple(sorted(simplex)), simplices)
+        simplices = [tuple(sorted(simplex)) for simplex in simplices]
         simplices = list(set(simplices))
 
         allpts = get_allpoints(simplices)
